4abc174549cdc1053276752dca061aaa8d94bed3
Ivan Prisyazhnyy <john.koepi@gmail.com>
2023-04-28 13:34:50 +0200

    add protocol serialization

--- src/protocol.zig
+++ src/protocol.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 
-// TODO: how to display struct gracefully (strings)?
 pub const Message = struct {
     src: []const u8,
     dest: []const u8,
@@ -15,7 +14,7 @@ pub const MessageBody = struct {
     msg_id: u64,
     in_reply_to: u64,
 
-    raw: []const u8,
+    raw: std.json.Value,
 
     pub usingnamespace MessageBodyMethods(@This());
 };
@@ -66,6 +65,39 @@ fn MessageMethods(comptime Self: type) type {
                 .body = MessageBody.init(),
             };
         }
+
+        pub fn format(value: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+            try writer.writeAll("{");
+            
+            var i: u32 = 0;
+            if (value.src.len > 0) {
+                try writer.writeAll(" \"src\": \"");
+                try writer.writeAll(value.src);
+                try writer.writeAll("\"");
+                i += 1;
+            } 
+
+            if (value.dest.len > 0) {
+                if (i > 0) try writer.writeAll(",");
+                try writer.writeAll(" \"dest\": \"");
+                try writer.writeAll(value.dest);
+                try writer.writeAll("\"");
+            } 
+
+            if (i > 0) try writer.writeAll(",");
+            try writer.writeAll(" \"body\": ");
+            try value.body.format(fmt, options, writer);
+            
+            try writer.writeAll(" }");
+        }
+
+        pub fn to_json_value(self: Self, alloc: std.mem.Allocator) !std.json.Value {
+            var v = std.json.Value{ .Object = std.json.ObjectMap.init(alloc)};
+            if (self.src.len > 0)  try v.Object.put("src",  std.json.Value{ .String = self.src });
+            if (self.dest.len > 0) try v.Object.put("dest", std.json.Value{ .String = self.dest });
+            try v.Object.put("body", try self.to_json_value(alloc));
+            return v;
+        }
     };
 }
 
@@ -76,6 +108,9 @@ fn MessageBodyMethods(comptime Self: type) type {
             const src = src0.?;
 
             self.typ = try_json_string(src.Object.get("type"));
+            self.msg_id = try_json_u64(src.Object.get("msg_id"));
+            self.in_reply_to = try_json_u64(src.Object.get("in_reply_to"));
+            self.raw = src;
 
             return self;
         }
@@ -85,8 +120,41 @@ fn MessageBodyMethods(comptime Self: type) type {
                 .typ = "",
                 .msg_id = 0,
                 .in_reply_to = 0,
-                .raw = "",
+                .raw = .Null,
             };
+        }
+
+        pub fn format(value: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+            _ = fmt;
+            _ = options;
+
+            try writer.writeAll("{");
+            
+            if (value.typ.len > 0) {
+                try writer.writeAll(" \"type\": ");
+                try std.json.stringify(value.typ, .{}, writer);
+            }
+
+            if (value.msg_id > 0) {
+                try writer.writeAll(", \"msg_id\": ");
+                try std.json.stringify(value.msg_id, .{}, writer);
+            }
+
+            if (value.in_reply_to > 0) {
+                try writer.writeAll(", \"in_reply_to\": ");
+                try std.json.stringify(value.in_reply_to, .{}, writer);
+            }
+
+            // TODO: merge raw
+
+            try writer.writeAll(" }");
+        }
+
+        pub fn to_json_value(self: Self, alloc: std.mem.Allocator) !std.json.Value {
+            var v = std.json.Value{ .Object = std.json.ObjectMap.init(alloc)};
+            if (self.typ.len > 0) try v.Object.put("type", std.json.Value{ .String = self.typ });
+            // TODO: ...
+            return v;
         }
     };
 }
@@ -96,5 +164,13 @@ fn try_json_string(val: ?std.json.Value) []const u8 {
     switch (val.?) {
         .String => |s| return s,
         else => return "",
+    }
+}
+
+fn try_json_u64(val: ?std.json.Value) u64 {
+    if (val == null) return 0;
+    switch (val.?) {
+        .Integer => |s| return @intCast(u64, s),
+        else => return 0,
     }
 }
